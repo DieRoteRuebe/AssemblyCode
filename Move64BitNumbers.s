6c765defe14d114fa7be480f8ecf6a6dd015a7c4
DieRoteRuebe <166912562+DieRoteRuebe@users.noreply.github.com>
2024-06-12 20:16:42 +0200

    Update Move64BitNumbers.s

--- Move64BitNumbers.s
+++ Move64BitNumbers.s
@@ -11,7 +11,7 @@ _start:
     mov rbx, 9223372036854775808    #Ab hier wird die Zahl negativ weil der sign byte "geschalten ist" -> 2^63 bit
     mov r9, 0x7FFFFFFFFFFFFFFF      #Das ist die selbe zahl wie in rax nur in Hex -> 0x7 wegen -1Byte = 8 bits (sign Byte)
     mov r10, r9                     #Kopieren der zahl aus r9 in r10
-    shr r10, 32                     #Bitshift um 32 bits nach rechts (31 plus sign bit) shr = arithmetische Bewegung nach rechts
+    shr r10, 32                     #Bitshift um 32 bits nach rechts (31 plus sign bit) shr = logische Bewegung nach rechts
     mov r11d, r9d                   #Kopieren der unteren 2^32 bits aus r9 in r11d -> r9d = 32 bit Register-Abschnitt von r9       
 #Achtung maximaler Mov von 32 bits:
     mov dword ptr [nArray], r10d    #Mov der 2^31 bits + sign bit aus r10 -> der untere teil r10d in das Array      
