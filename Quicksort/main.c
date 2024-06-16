2fe1eb3c2ec0d7fdeab1635f94995bc90bb69a9a
DieRoteRuebe <166912562+DieRoteRuebe@users.noreply.github.com>
2024-06-16 22:05:54 +0200

    Add files via upload

---
+++ Quicksort/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+extern int* quickSort(int* inArray, int size, int start, int end)
+
+__attribute__((naked)); int main()
+{
+  int inArray[] = { 5, 4, 8, 10, 99,3,12,1,6, 1002, 1008, 1990, 20315, 78941};
+  int arraySize = sizeof(inArray)/4;
+  int start = 0;
+  int end = arraySize-1;
+  printf("%d", arraySize);
+  printf("\n");
+  for(int i = 0; i < arraySize; i+=1)
+  {
+    printf("%d", inArray[i]);
+    printf("\n");
+  }
+  printf("\n");
+  quickSort(inArray, arraySize, start, end);
+  arraySize = sizeof(inArray)/4;
+  for(int i = 0; i < arraySize; i+=1)
+  {
+    printf("%d", inArray[i]);
+    printf("\n");
+  }
+
+  
+  return 0;
+}
